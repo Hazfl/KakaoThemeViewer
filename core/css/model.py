7dfca4463d97c5aa4f7363d5c150c2c291d85eb1
Hazfl <towpeoplea@gmail.com>
2026-07-06 15:11:37 +0900

    Create model.py

---
+++ core/css/model.py
@@ -0,0 +1,54 @@
+from __future__ import annotations
+
+from dataclasses import dataclass, field
+
+
+@dataclass(slots=True)
+class CssValue:
+    raw: str
+
+
+@dataclass(slots=True)
+class CssImageValue(CssValue):
+    filename: str = ""
+    relative_path: str | None = None
+
+
+@dataclass(slots=True)
+class CssColorValue(CssValue):
+    color: str = ""
+
+
+@dataclass(slots=True)
+class CssProperty:
+    name: str
+    value: CssValue
+
+
+@dataclass(slots=True)
+class CssRule:
+    selector: str
+
+    properties: list[CssProperty] = field(default_factory=list)
+
+    def add(
+        self,
+        name: str,
+        value: CssValue,
+    ):
+
+        self.properties.append(
+            CssProperty(
+                name=name,
+                value=value,
+            )
+        )
+
+    def get(self, name: str):
+
+        for prop in self.properties:
+
+            if prop.name == name:
+                return prop.value
+
+        return None
